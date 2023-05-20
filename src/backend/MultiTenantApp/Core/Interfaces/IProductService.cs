930cf622e5cd0510f7d2a64dc413ccdc4ab88930
Chamath Viduranga <chamath.viduranga@airudi.com>
2023-05-20 12:33:44 +0530

    created

---
+++ src/backend/MultiTenantApp/Core/Interfaces/IProductService.cs
@@ -0,0 +1,16 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Interfaces
+{
+    public interface IProductService
+    {
+        Task<Product> CreateAsync(string name, string description, int rate);
+        Task<Product> GetByIdAsync(int id);
+        Task<IReadOnlyList<Product>> GetAllAsync();
+    }
+}
---
+++ src/backend/MultiTenantApp/Core/Options/TenantSettings.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Options
+{
+    public class TenantSettings
+    {
+        public Configuration Defaults { get; set; }
+        public List<Tenant> Tenants { get; set; }
+    }
+    public class Tenant
+    {
+        public string Name { get; set; }
+        public string TID { get; set; }
+        public string ConnectionString { get; set; }
+    }
+    public class Configuration
+    {
+        public string DBProvider { get; set; }
+        public string ConnectionString { get; set; }
+    }
+}
